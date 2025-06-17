4f2500d4bdceaa96054f3473dc151553b8c6bafc
Alfonso Ramírez <a24rameal@iesgrancapitan.org>
2025-06-18 00:17:53 +0200

    Creacion  UserModel

--- src/data/mongodb/index.ts
+++ src/data/mongodb/index.ts
@@ -1,1 +1,5 @@
+// Exportamos la conexion de mongodb
 export * from "./mongo-database";
+
+// Exportamos el modelo de usuario
+export * from "./models/user.model";
---
+++ src/data/mongodb/models/user.model.ts
@@ -0,0 +1,30 @@
+// Fichero que tiene el modelo del usuario
+
+// Importamos mongoose
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Email is required"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  role: {
+    type: [String],
+    default: ["USER_ROLE"],
+    enum: ["USER_ROLE", "ATHLETE_ROLE", "COACH_ROLE", "CLUB_ROLE", "ADMIN_PTS"],
+  },
+  avatar: {
+    type: String,
+  },
+});
+
+export const UserModel = mongoose.model("User", userSchema);
