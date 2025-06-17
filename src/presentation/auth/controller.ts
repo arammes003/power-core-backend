690228d84ae0bce4e6edbdcddc440b1c6f715cc7
Alfonso Ramírez <a24rameal@iesgrancapitan.org>
2025-06-18 01:19:35 +0200

    Manejo de errores

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,11 +1,18 @@
 // Fichero que contiene todas las funciones de autenticacion del usuario
-import { Request, Response } from "express";
-import { AuthRepository, RegisterUserDto } from "../../domain";
-import { log } from "node:console";
+import e, { Request, Response } from "express";
+import { AuthRepository, CustomError, RegisterUserDto } from "../../domain";
 
 export class AuthController {
   // Inyeccion de dependencias
   constructor(private readonly authRepository: AuthRepository) {}
+
+  private handleError = (error: unknown, res: Response) => {
+    if (error instanceof CustomError)
+      return res.status(error.statusCode).json({ error: error.message });
+
+    console.log(error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  };
 
   registerUser = async (req: Request, res: Response) => {
     const [error, registerUserDto] = RegisterUserDto.create(req.body);
@@ -14,7 +21,7 @@ export class AuthController {
       this.authRepository
         .register(registerUserDto!)
         .then((user) => res.json(user))
-        .catch((error) => res.status(500).json(error));
+        .catch((error) => this.handleError(error, res));
   };
 
   loginUser = (req: Request, res: Response) => {
