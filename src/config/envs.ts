9cbeb0da96cc5ec79577c8ba6d8d39fd5cfb8fae
Alfonso Ramírez <a24rameal@iesgrancapitan.org>
2025-06-18 15:49:26 +0200

    Middleware validacion token

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,4 +3,5 @@ export const envs = {
   PORT: Number(process.env.PORT),
   MONGO_URL: process.env.MONGO_URL || "",
   MONGO_DB_NAME: process.env.MONGO_DB_NAME || "",
+  JWT_SEED: process.env.JWT_SEED || "",
 };
--- src/config/jwt.ts
+++ src/config/jwt.ts
@@ -1,16 +1,30 @@
 // Fichero que verifica y firma con jwt
 import jwt, { SignOptions } from "jsonwebtoken";
+import { envs } from "./envs";
+
+const JWT_SEED = envs.JWT_SEED;
 
 export class JwtAdapter {
+  // Metodo que genera un token
   static async generateToken(
     payload: Object,
     duration: SignOptions["expiresIn"] = "2h"
   ): Promise<string | null> {
     return new Promise((resolve) => {
       // todo: generacion de la semilla
-      jwt.sign(payload, "SEED", { expiresIn: duration }, (error, token) => {
+      jwt.sign(payload, JWT_SEED, { expiresIn: duration }, (error, token) => {
         if (error) return resolve(null);
         resolve(token as string);
+      });
+    });
+  }
+
+  // Metodo que verifica un token
+  static async validateToken<T>(token: string): Promise<T | null> {
+    return new Promise((resolve) => {
+      jwt.verify(token, JWT_SEED, (error, decoded) => {
+        if (error) return resolve(null);
+        resolve(decoded as T);
       });
     });
   }
