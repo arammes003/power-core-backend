db306f34054f124a8863dd36ca121daf435f8dbb
Alfonso Ramírez <a24rameal@iesgrancapitan.org>
2025-06-28 18:25:49 +0200

    Club model

---
+++ src/data/mongodb/models/club.model.ts
@@ -0,0 +1,68 @@
+import mongoose, { Schema } from "mongoose";
+
+const clubSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    unique: true,
+  },
+  description: {
+    type: String,
+  },
+  club_logo: {
+    type: String,
+    required: [true, "Club logo is required"],
+  },
+  ac_name: {
+    type: String,
+    required: [true, "Autonomous Community is required"],
+  },
+  prov_name: {
+    type: String,
+    required: [true, "Province is required"],
+  },
+  city_name: {
+    type: String,
+    required: [true, "City is required"],
+  },
+  contact_info: {
+    phone: {
+      type: String,
+      required: [true, "Phone is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+    },
+    website: {
+      type: String,
+    },
+    social_media: {
+      instagram: String,
+      facebook: String,
+      youtube: String,
+    },
+  },
+  admin: {
+    type: Schema.Types.ObjectId,
+    required: [true, "Admin is required"],
+    ref: "Users",
+  },
+  coaches: {
+    type: Schema.Types.ObjectId,
+    ref: "Coaches",
+  },
+  athletes: {
+    type: Schema.Types.ObjectId,
+    ref: "Athletes",
+  },
+  membership_price: {
+    type: Number,
+    required: [true, "Membership price is required"],
+  },
+  created_at: {
+    type: Date,
+  },
+});
+
+export const ClubModel = mongoose.model("Club", clubSchema);
